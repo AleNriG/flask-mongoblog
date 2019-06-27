5285fc8dc4f7be9a0ded5ca092ec9f3ccf5a3ac0
Aleksandr Gorokhov <agorokhov94@gmail.com>
2019-06-27 12:24:14 +0300

    Add html template for the main paige

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
+from flask import render_template
+
 from . import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, User!"
+    user = {"username": "Master"}
+    return render_template("index.html", title="Home", user=user)

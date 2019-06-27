55c891e24115b7563e9dbef21d8845531ed528aa
Aleksandr Gorokhov <agorokhov94@gmail.com>
2019-06-27 22:42:37 +0300

    Add db to app init

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_mongoengine import MongoEngine
 
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config["MONGODB_SETTINGS"] = {"DB": "my_blog_log"}
+
+db = MongoEngine(app)
 
 from . import routes
+
+
+if __name__ == "__main__":
+    app.run()

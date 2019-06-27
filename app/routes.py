b1fbcb54e0e6df9a30f8cb379fe29b003b130fab
Aleksandr Gorokhov <agorokhov94@gmail.com>
2019-06-27 14:57:38 +0300

    Connect login form to login.html

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
 
 from . import app
+from app.forms import LoginForm
 
 
 @app.route("/")
@@ -8,13 +9,12 @@ from . import app
 def index():
     user = {"username": "Master"}
     posts = [
-        {
-            "author": {"username": "Pirate"},
-            "body": "Are you ready, kids?!"
-        },
-        {
-            "author": {"username": "Kids"},
-            "body": "YES!!!"
-        }
+        {"author": {"username": "Pirate"}, "body": "Are you ready, kids?!"},
+        {"author": {"username": "Kids"}, "body": "YES!!!"},
     ]
     return render_template("index.html", user=user, posts=posts)
+
+
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Sign In", form=form)

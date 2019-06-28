a2aea75e1f94247e1ea1bbb8d366bf6a9a585583
Aleksandr Gorokhov <agorokhov94@gmail.com>
2019-06-28 09:29:05 +0300

    Add logout route

--- app/routes.py
+++ app/routes.py
@@ -4,10 +4,11 @@ from flask import render_template
 from flask import url_for
 from flask_login import current_user
 from flask_login import login_user
+from flask_login import logout_user
 
-from . import app
 from app.forms import LoginForm
 from app.models import User
+from . import app
 
 
 @app.route("/")
@@ -34,3 +35,9 @@ def login():
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for("index"))
     return render_template("login.html", title="Sign In", form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for("index"))

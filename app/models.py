1d9cd559f3aafdbc8d90dc57b19fdb1b57322e1a
Aleksandr Gorokhov <agorokhov94@gmail.com>
2019-06-28 08:29:23 +0300

    Add post model

--- app/models.py
+++ app/models.py
@@ -1,17 +1,22 @@
+import datetime
+
 from app import db
-
-
-class Comment(db.Document):
-    pass
-
-
-class Post(db.Document):
-    pass
 
 
 class User(db.Document):
     email = db.StringField(max_lenght=32, required=True)
     username = db.StringField(max_lenght=64, required=True)
     password_hash = db.StringField(max_lenght=128, required=True)
-    posts = db.EmbeddedDocumentListField(Post, default=[])
-    comments = db.EmbeddedDocumentListField(Comment, default=[])
+
+
+class Post(db.Document):
+    title = db.StringField(max_length=64, required=True)
+    content = db.StringField(required=True)
+    publication_datetime = db.DateTimeField(
+        default=datetime.datetime.now, required=True
+    )
+    author_id = db.ReferenceField(User)
+
+
+class Comment(db.Document):
+    pass

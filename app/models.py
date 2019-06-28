56cdc45d09424a64c30b2f46c03a6677f4a7d9fd
Aleksandr Gorokhov <agorokhov94@gmail.com>
2019-06-28 07:58:58 +0300

    Change max length of user fields

--- app/models.py
+++ app/models.py
@@ -1,11 +1,7 @@
-import datetime
-
-from flask import url_for
-
 from app import db
 
 
 class User(db.Document):
     email = db.StringField(max_lenght=32, required=True)
-    username = db.StringField(max_lenght=32, required=True)
-    password = db.StringField(max_lenght=32, required=True)
+    username = db.StringField(max_lenght=64, required=True)
+    password_hash = db.StringField(max_lenght=128, required=True)

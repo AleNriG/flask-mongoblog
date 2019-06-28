9bbfc6a151915f1025847f60baea6eca8a47874d
Aleksandr Gorokhov <agorokhov94@gmail.com>
2019-06-28 11:09:29 +0300

    Return to EmdeddedDocument models

--- app/models.py
+++ app/models.py
@@ -3,26 +3,25 @@ import datetime
 from app import db
 
 
+class Comment(db.EmbeddedDocument):
+    title = db.StringField(max_length=64, required=True)
+    content = db.StringField(required=True)
+    publication_datetime = db.DateTimeField(
+        default=datetime.datetime.now, required=True
+    )
+
+
+class Post(db.EmbeddedDocument):
+    title = db.StringField(max_length=64, required=True)
+    content = db.StringField(required=True)
+    publication_datetime = db.DateTimeField(
+        default=datetime.datetime.now, required=True
+    )
+
+
 class User(db.Document):
     email = db.StringField(max_lenght=32, required=True)
     username = db.StringField(max_lenght=64, required=True)
     password_hash = db.StringField(max_lenght=128, required=True)
-
-
-class Post(db.Document):
-    title = db.StringField(max_length=64, required=True)
-    content = db.StringField(required=True)
-    publication_datetime = db.DateTimeField(
-        default=datetime.datetime.now, required=True
-    )
-    author_id = db.ReferenceField(User)
-
-
-class Comment(db.Document):
-    title = db.StringField(max_length=64, required=True)
-    content = db.StringField(required=True)
-    publication_datetime = db.DateTimeField(
-        default=datetime.datetime.now, required=True
-    )
-    post_id = db.ReferenceField(Post)
-    author_id = db.ReferenceField(User)
+    posts = db.EmbeddedDocumentListField(Post, default=[])
+    comments = db.EmbeddedDocumentListField(Comment, default=[])

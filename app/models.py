9ef4783548dee72eb9c8d85bdccb67379fc06a70
Aleksandr Gorokhov <agorokhov94@gmail.com>
2019-06-28 08:04:01 +0300

    Add mock post and comment documents

--- app/models.py
+++ app/models.py
@@ -1,7 +1,17 @@
 from app import db
 
 
+class Comment(db.Document):
+    pass
+
+
+class Post(db.Document):
+    pass
+
+
 class User(db.Document):
     email = db.StringField(max_lenght=32, required=True)
     username = db.StringField(max_lenght=64, required=True)
     password_hash = db.StringField(max_lenght=128, required=True)
+    posts = db.EmbeddedDocumentListField(Post, default=[])
+    comments = db.EmbeddedDocumentListField(Comment, default=[])
